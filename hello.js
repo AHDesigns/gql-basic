5fce886161e2dd90651ae84cfd9aad3e8900a2ec
Adam <adam.hall@sky.uk>
2017-11-07 08:14:33 +0000

    basic gql express server

---
+++ hello.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: Int
+  }
+`);
+
+var root = { hello: () => 3};
+
+var app = express();
+
+app.use('/graphql', graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+}));
+
+app.listen(1212);
+console.log('Running gql at http://localhost:1212/graphql');
